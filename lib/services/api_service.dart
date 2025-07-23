185e8e17daed049486dc9cc061a4df3f0b1d2fec
Kai Ott <kai.ott@bruker.com>
2025-07-23 16:26:53 +0200

    implement fetchLesson async function

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:asvz_autosignup/models/lesson.dart';
 import 'package:http/http.dart' as http;
 
 const String baseUrl = 'schalter.asvz.ch';
 
-Lesson? fetchLesson(int id) {
+Future<Lesson> fetchLesson(int id) async {
   final resourcePath = '/tn-api/api/Lessons/$id';
   final url = Uri.https(baseUrl, resourcePath);
-  return null;
+  final response = await http.get(url);
+  if (response.statusCode != 200) {
+    throw HttpException("Non-okay status code from request.", uri: url);
+  }
+  final jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+  final lesson = Lesson.fromJson(jsonResponse["data"]);
+  return lesson;
 }
